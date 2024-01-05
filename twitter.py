3fd3034867795d9a3ce21a1b0abc6bbe8db4e1da
liiran <ashbel.liran@gmail.com>
2024-01-04 21:19:59 -0500

    Twitter + Youtube Fixes

---
+++ twitter.py
@@ -0,0 +1,41 @@
+import tweepy
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# consumer key + secret and Access token + secret in order to use twitter API
+consumer_key = "1GLgxcynNAsb7BmdkFRYmSJS0"
+consumer_secret = "UM2tUv6QPrFVxL35dSnKw7Y7se6VeC88KJMzWhnlKuVoPCRprK"
+access_token = "1204954626912661504-lQ9U1EXNzMTR00gkmGqI4ndEwQU86q"
+access_token_secret = "r9LVr0Hd7btNLL8VEDDiPrEf2jzgDoszmy26Y8kvzUtMT"
+
+# using tweepy imported library for python and twitter API connection
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+search_query = "'ref''Engineer'-filter:retweets AND -filter:replies AND -filter:links"
+numberTweets = 10
+
+tweets_df = pd.DataFrame()
+
+try:
+    # the number of tweets we want to retrieved from the search
+    tweets = api.search_tweets(q=search_query, lang="en", count= numberTweets, tweet_mode ='extended')
+    
+    # choosing certain attributes relating to the tweet
+    attributes_container = [[tweet.user.name, tweet.source, tweet.full_text] for tweet in tweets]
+    columns = ["User", "Source of Tweet", "Tweet"]
+    
+    # create a dataframe with the tweet data
+    tweets_df = pd.DataFrame(attributes_container, columns=columns)
+
+except BaseException as e:
+    print('Status Failed On,',str(e))
+     
+print(tweets_df)
---
+++ youtube.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+
+options = Options()
+#options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.google.com/")
+
+# search input xpath for youtube
+# search_input = driver.find_element("xpath", "//input[@id='search']")
+
+# search input xpath for google
+search_input = driver.find_element("xpath", "//*[@id='APjFqb']")
+
+# various search filters
+inputs = [" ", "Civil Engineering", "Plumber", "Astronaut"]
+input_addOns = [" ", "#DayInTheLife", " #OfficeLife", " #BehindTheDesk", " #OnTheJob", " #MyWorkDay"]
+input_location = [" ", " Canada", " Saudi Arabia", " USA"]
+input_gender = [" ", " Male", " Female"]
+
+# use a set to store unique links to avoid duplicates
+unique_links = set()
+
+for a in range(1):
+    search_input.clear()  # Delete the current stuff in search input field
+    search_input.send_keys('site:youtube.com/ "' + inputs[1] + '" "' + input_addOns[1] + '"')
+
+    # same line with additional filters for testing
+    # search_input.send_keys('site:youtube.com/ "' + inputs[1] + '" "' + input_addOns[1] + '" "' + input_location[1].lstrip() + '" "' + input_gender[1].lstrip() + '"')
+    
+    # Wait until the input field has a non-empty value
+    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(("xpath", "//*[@id='APjFqb']"), ''))
+    search_input.send_keys(Keys.RETURN)
+
+    links = driver.find_elements("xpath", "//a[@href and contains(@href, '/watch?v=') and not(contains(@href, 'googleads'))]")
+
+    for link in links:
+       # get the link and then use regular expression to 'match' the base link (removing time stamps and duplicate links)
+        href = link.get_attribute("href")
+        match_link = re.match(r'https://www\.youtube\.com/watch\?v=[^&]+', href)
+
+        if match_link:
+            base_href = match_link.group()
+
+            if base_href not in unique_links:
+                unique_links.add(base_href)
+                print(base_href)
+        else:
+        # if re.match returns None, add the link directly to unique_links
+            if href not in unique_links:
+                unique_links.add(href)
+                print(href)
+
+# driver.quit()
