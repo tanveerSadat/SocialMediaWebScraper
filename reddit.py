46b0ee0418520d84db6a7d7ba77e7fe04348f5b7
liiran <ashbel.liran@gmail.com>
2024-01-05 21:04:28 -0500

    Changes to youtube and reddit scraping

--- reddit.py
+++ reddit.py
@@ -2,23 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 def scrape_reddit(query):
 
     options = Options()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    # Construct the search URL
+    # Construct and navigate to the search URL
     search_url = f"https://www.reddit.com/search/?q={query.replace(' ', '%20')}"
-
-    # Navigate to the search URL
     driver.get(search_url)
-    time.sleep(5) 
 
     # Find links on the search results page
     links = driver.find_elements("css selector", "a[href]")
@@ -29,7 +25,6 @@ def scrape_reddit(query):
     for href in reddit_post_links:
         print(href)
 
-    time.sleep(2)
     driver.quit()
 
 # Get user input for the search query
--- youtube.py
+++ youtube.py
@@ -8,58 +8,65 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 
-options = Options()
-#options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_experimental_option("detach", True)
+def scrape_youtube(input):
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_experimental_option("detach", True)
 
-driver.get("https://www.google.com/")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# search input xpath for youtube
-# search_input = driver.find_element("xpath", "//input[@id='search']")
+    driver.get("https://www.google.com/")
 
-# search input xpath for google
-search_input = driver.find_element("xpath", "//*[@id='APjFqb']")
+    # search input xpath for youtube
+    # search_input = driver.find_element("xpath", "//input[@id='search']")
 
-# various search filters
-inputs = [" ", "Civil Engineering", "Plumber", "Astronaut"]
-input_addOns = [" ", "#DayInTheLife", " #OfficeLife", " #BehindTheDesk", " #OnTheJob", " #MyWorkDay"]
-input_location = [" ", " Canada", " Saudi Arabia", " USA"]
-input_gender = [" ", " Male", " Female"]
+    # search input xpath for google
+    search_input = driver.find_element("xpath", "//*[@id='APjFqb']")
 
-# use a set to store unique links to avoid duplicates
-unique_links = set()
+    # various search filters
+    inputs = [" ", "Civil Engineering", "Plumber", "Astronaut"]
+    input_addOns = [" ", "#DayInTheLife", " #OfficeLife", " #BehindTheDesk", " #OnTheJob", " #MyWorkDay"]
+    input_location = [" ", " Canada", " Saudi Arabia", " USA"]
+    input_gender = [" ", " Male", " Female"]
 
-for a in range(1):
-    search_input.clear()  # Delete the current stuff in search input field
-    search_input.send_keys('site:youtube.com/ "' + inputs[1] + '" "' + input_addOns[1] + '"')
+    # use a set to store unique links to avoid duplicates
+    unique_links = set()
+    youtube_links = ""
 
-    # same line with additional filters for testing
-    # search_input.send_keys('site:youtube.com/ "' + inputs[1] + '" "' + input_addOns[1] + '" "' + input_location[1].lstrip() + '" "' + input_gender[1].lstrip() + '"')
-    
-    # Wait until the input field has a non-empty value
-    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(("xpath", "//*[@id='APjFqb']"), ''))
-    search_input.send_keys(Keys.RETURN)
+    for a in range(1):
+        search_input.clear()  # Delete the current stuff in search input field
+        search_input.send_keys('site:youtube.com/ "' + input + '" "' + input_addOns[1] + '"')
+        
+        # Wait until the input field has a non-empty value
+        WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(("xpath", "//*[@id='APjFqb']"), ''))
+        search_input.send_keys(Keys.RETURN)
 
-    links = driver.find_elements("xpath", "//a[@href and contains(@href, '/watch?v=') and not(contains(@href, 'googleads'))]")
+        links = driver.find_elements("xpath", "//a[@href and contains(@href, '/watch?v=') and not(contains(@href, 'googleads'))]")
 
-    for link in links:
-       # get the link and then use regular expression to 'match' the base link (removing time stamps and duplicate links)
-        href = link.get_attribute("href")
-        match_link = re.match(r'https://www\.youtube\.com/watch\?v=[^&]+', href)
+        for link in links:
+        # get the link and then use regular expression to 'match' the base link (removing time stamps and duplicate links)
+            href = link.get_attribute("href")
+            match_link = re.match(r'(https://(www|m)\.youtube\.com/watch\?v=[^&]+)', href)
 
-        if match_link:
-            base_href = match_link.group()
+            if match_link:
+                base_href = match_link.group()
 
-            if base_href not in unique_links:
-                unique_links.add(base_href)
-                print(base_href)
-        else:
-        # if re.match returns None, add the link directly to unique_links
-            if href not in unique_links:
-                unique_links.add(href)
-                print(href)
+                if base_href not in unique_links:
+                    unique_links.add(base_href)
+                    youtube_links = youtube_links + base_href + " "
+                    print(base_href)
+            else:
+            # if re.match returns None, add the link directly to unique_links
+                if href not in unique_links:
+                    unique_links.add(href)
+                    youtube_links = youtube_links + href + " "
+                    print(href)
 
-# driver.quit()
+    return youtube_links
+    driver.quit()
+
+input = "Software Tester"
+result_links = scrape_youtube(input)
+print(result_links)
